88f0df4f2708bb900443bbf501d998f9f736695c
seanlutz <seanlutz@outlook.com>
2018-10-07 22:02:14 +0000

    Made TicTacToe Server

--- src/main/java/com/sean/tictac/Interface/structs/BoardStatus.java
+++ src/main/java/com/sean/tictac/Interface/structs/BoardStatus.java
@@ -1,9 +1,15 @@
 package com.sean.tictac.Interface.structs;
 
 public enum BoardStatus {
-    WINX,
-    WINY,
-    DRAW,
-    TURNX,
-    TURNY
+    WINX(Player.X, true),
+    WINO(Player.O, true),
+    DRAW(null, true),
+    TURNX(Player.X, false),
+    TURNO(Player.O, false);
+    public final Player player;  
+    public final Boolean isOver;
+    BoardStatus(Player player, Boolean isOver) {
+        this.player = player;
+        this.isOver = isOver;
+    }
 }
--- src/main/java/com/sean/tictac/TheBoard.java
+++ src/main/java/com/sean/tictac/TheBoard.java
@@ -1,6 +1,8 @@
 package com.sean.tictac;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -12,42 +14,113 @@ import com.sean.tictac.Interface.structs.Space;
 public class TheBoard implements Board {
     Map<Space,Player> moves; 
     BoardStatus status;
+    Set<Space> remainingMoves; 
 
     public TheBoard(){
-        moves = new HashMap<Space,Player>();
-        status = BoardStatus.TURNX;
+        resetBoard();
     }
 
+    public void resetBoard() {
+        moves = new HashMap<Space,Player>();
+        status = BoardStatus.TURNX;
+        remainingMoves = new HashSet<Space>(Arrays.asList(Space.values()));
+	}
+
     public Boolean isOver() {
-        return over;
+        return status.isOver;
     }
 
     public BoardStatus getStatus() {
-        if{}
-        return null;
+        return status;
     }
 
     public Player currentTurn() {
-        return turn;
+        switch(status){
+            case TURNX: return Player.X;
+            case TURNO: return Player.O;
+            default: return null;
+        }
     }
 
     public Set<Space> getValidMoves() {
-        return null;
+        return isOver() ? new HashSet<Space>(): remainingMoves;
     }
 
     public Boolean makeMove(Player player, Space space) {
-        
+        if(!getValidMoves().contains(space)){
+            return false;
+        }
+        else{
+            moves.put(space, player);
+            remainingMoves.remove(space);
+            status = checkWinner(status);
+            switch(status){
+                case TURNX: status=BoardStatus.TURNO;
+                case TURNO: status=BoardStatus.TURNX;
+                default:;
+            }
+            return true;
+        }
+    }
+    public Map<Space, Player> cereal() {
+        return moves;
+    }
+
+    private BoardStatus checkWinner(BoardStatus status) {
+        Player horiz = checkHoriz();
+        if (horiz != null){
+            return horiz == Player.X ? BoardStatus.WINX : BoardStatus.WINO;
+        }
+        Player vert = checkVert();
+        if (vert != null){
+            return vert == Player.X ? BoardStatus.WINX : BoardStatus.WINO;
+        }
+        Player diag = checkDiag();
+        if (diag != null){
+            return diag == Player.X ? BoardStatus.WINX : BoardStatus.WINO;
+        }
+        if(moves.size() == 9){
+            return BoardStatus.DRAW;
+        }
+        return status;
+    }
+    private Player check(Space a, Space b, Space c){
+        if (moves.get(a) == moves.get(b)
+         && moves.get(b) == moves.get(c) 
+         && moves.get(c) != null)
+         {
+             return moves.get(c);
+         }
+         else{
+              return null;}
+    }
+
+	private Player checkHoriz() {
+        Player row1 = check(Space.A, Space.B, Space.C);
+        if(row1 != null){return row1;}
+        Player row2 = check(Space.D, Space.E, Space.F);
+        if(row2 != null){return row2;}
+        Player row3 = check(Space.G, Space.H, Space.I);
+        if(row3 != null){return row3;}
         return null;
     }
 
-    @Override
-    public Map<Space, Player> cereal() {
+    private Player checkVert() {
+        Player row1 = check(Space.A, Space.D, Space.G);
+        if(row1 != null){return row1;}
+        Player row2 = check(Space.B, Space.E, Space.H);
+        if(row2 != null){return row2;}
+        Player row3 = check(Space.C, Space.F, Space.I);
+        if(row3 != null){return row3;}
         return null;
-	}
+    }
 
-	@Override
-	public void resetBoard() {
-		
-	}
+    private Player checkDiag(){
+        Player row1 = check(Space.A, Space.E, Space.I);
+        if(row1 != null){return row1;}
+        Player row2 = check(Space.C, Space.E, Space.G);
+        if(row2 != null){return row2;}
+        return null;
+    }
 
 }
